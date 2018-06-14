a26b41f682be157a834e684b61dd439322bc54f8
salody <salodyliu@gmail.com>
2018-06-14 09:08:07 +0800

    处理401的错误

--- src/api/battles-api.js
+++ src/api/battles-api.js
@@ -9,7 +9,17 @@ function getPublicStartupBattles () {
 
 function getPrivateStartupBattles () {
   const url = `${BASE_URL}/api/battles/private`
-  return axios.get(url).then(response => response.data)
+  return axios.get(url)
+    .then(response => response.data)
+    .catch((err) => {
+      if (err.toString().includes('401')) {
+        return axios.get(url, {
+          headers: {
+            'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6IlFqZENSRUkzUVVORU5UbENNelF3TlVFd01rWkVNVEk1TmtaQlJrVkROakZHUkVSQk1UQkZNQSJ9.eyJpc3MiOiJodHRwczovL3NhbG9keS5hdXRoMC5jb20vIiwic3ViIjoiaDdMQ1NBMXM1NVp6eTBSeU1TN2g2QkQ0R0R2anRGdjhAY2xpZW50cyIsImF1ZCI6Imh0dHBzOi8vc2Fsb2R5LmNjIiwiaWF0IjoxNTI4OTMwODUzLCJleHAiOjE1MjkwMTcyNTMsImF6cCI6Img3TENTQTFzNTVaenkwUnlNUzdoNkJENEdEdmp0RnY4IiwiZ3R5IjoiY2xpZW50LWNyZWRlbnRpYWxzIn0.Sf9KV5V2hZviiFuweeKPCkp6PDFt9kU6C0LU0Fw0oEETnPTN8PEeCsLtQ4fnKezRGGxyOBSJx4kGflSI4yeh3JrgN2akLzMEZMU5f7ZaBGjN4UKd5-ljojIYj2AAbCtjb9z7Ca1oZbDJXiSrAgplTNxGcJE63gq_7mtAsMhqNSUzcMU2o5ZYhhoCcSiu9TN4OjBEBc7aEtO7a2DXtIFFc8mwDVYG-RauU0fJKlIEQ8KXB3HRpU_RBa5ix2Qj2c0qB5VagL_yRDbqQO1Mx4ELZfdhPmxPAS3z3aM1hwbdHsBnfohcD2D6WWQmx2ElHe18dOI84N-scolCLVsEPTV9Cg'
+          }
+        }).then(response => response.data)
+      }
+    })
 }
 
 export {getPublicStartupBattles, getPrivateStartupBattles}
--- src/views/battles/PublicBattles.vue
+++ src/views/battles/PublicBattles.vue
@@ -1,12 +1,57 @@
 <template>
   <div>
-    public
+    <h3 class="text-center">Daily Startup Battles</h3>
+    <hr/>
+    <spinner :loading="isLoading"></spinner>
+    <div class="col-sm-4" v-for="battle in publicBattles" :key="battle.id">
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <h3 class="panel-title"> {{ battle.name }} </h3>
+        </div>
+        <div class="panel-body">
+          <p><span class="badge alert-info"> Sponsor: </span> {{ battle.sponsor }} </p>
+          <p><span class="badge alert-danger"> SeedFund: </span><strong> ${{ battle.seedFund }} </strong></p>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-sm-12" v-show="!isLoading">
+      <div class="jumbotron text-center">
+        <h2>View Private Startup Battles</h2>
+        <router-link class="btn btn-lg btn-success" to="/battles/private">Private Startup Battles</router-link>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import { isLoggedIn } from '@/utils/auth'
+import { getPublicStartupBattles } from '@/api/battles-api'
+import Spinner from '@/components/Spinner'
 export default {
-  name: 'PublicBattles'
+  name: 'PublicBattles',
+  components: {Spinner},
+  data () {
+    return {
+      publicBattles: [],
+      isLoading: false
+    }
+  },
+  methods: {
+    isLoggedIn () {
+      return isLoggedIn()
+    },
+    getPublicStartupBattles () {
+      this.isLoading = true
+      getPublicStartupBattles().then((battles) => {
+        this.publicBattles = battles
+        this.isLoading = false
+      })
+    }
+  },
+  mounted () {
+    this.getPublicStartupBattles()
+  }
 }
 </script>
 
